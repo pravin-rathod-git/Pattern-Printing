0fc2d746f117d9af675a1afa0093cec8fc3bc543
pravin-rathod-git <pr819270@gmail.com>
2025-11-30 23:58:25 +0530

    InvertedLefttHalfPyramid pattern

---
+++ InvertedLefttHalfPyramid.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class InvertedLefttHalfPyramid {
+    public static void main(String[] args)
+    {
+
+        // Number of rows
+        int N = 5;
+
+        // Outer loop runs N times, once for each row
+        for (int i = 1; i <= N; i++) {
+            // Inner loop prints 'i - 1' spaces
+            for (int j = 1; j <= i - 1; j++) {
+                System.out.print("  ");
+            }
+              
+              // Inner loop prints 'N - i + 1' stars
+            for (int j = 1; j <= N - i + 1; j++) {
+                System.out.print("*");
+            }
+            // Move to the next line
+            System.out.println();
+        }
+    }
+}
